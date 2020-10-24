aabfa3566d610e645fc296fd5970c270eca915db
Luc Woodman <woodmanl1@cardiff.ac.uk>
2020-10-24 15:34:19 +0100

    Update player.py

--- main_game/player.py
+++ main_game/player.py
@@ -1,7 +1,42 @@
 from items import *
 from map import rooms
 
-inventory = [item_id, item_laptop, item_money]
+from items import *
+from map import rooms
+
+#inventory = [item_id, item_laptop, item_money]
 
 # Start game at the reception
-current_room = rooms["Reception"]
+#current_room = rooms["Reception"]
+
+player_character = {
+    "name": "Character1",
+    "Occupation": "fill in",
+    "Strength": "100",
+    "Oxygen": "123",
+    "Energy": "100",
+    "inventory": []
+
+player_character1 = {
+    "name": "Character1",
+    "Occupation": "fill in",
+    "Strength": "100",
+    "Oxygen": "123",
+    "Energy": "100",
+    "inventory": []
+
+player_character2 = {
+    "name": "Character1",
+    "Occupation": "fill in",
+    "Strength": "100",
+    "Oxygen": "123",
+    "Energy": "100",
+    "inventory": []
+    
+characters = {
+
+    "Character" : player_character,
+    "Character1": player_character1,
+    "Character2": player_character2
+
+}
