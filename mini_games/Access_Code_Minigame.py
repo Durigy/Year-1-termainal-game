211b81b9beb7ce4e2b3090b390ca7ac1b5d0afdb
Luc Woodman <woodmanl1@cardiff.ac.uk>
2020-10-24 13:13:26 +0100

    Fixed bug in Access_Code_Minigame.py

--- mini_games/Access_Code_Minigame.py
+++ mini_games/Access_Code_Minigame.py
@@ -20,36 +20,39 @@ def access_code_minigame():
 
         #Loop that checks whether the users guesses remains below 3
         #and that they do not enter a value outside of 1-5.
-        while guesses < 5:  
-            guess = int(input("A guess between 1-5, shouldn't be too hard?:"))
-            if guess > 5:
-                print("Hmmm, could've sworn there was only five numbers on the keypad" + "\n")
-                guesses +=1
-            elif guess < 1:
-                print("Didn't know you could have negative numbers on a keypad" + "\n")
-                guesses +=1
-            else:
+        while guesses < 5:
+            try:
+                guess = int(input("A guess between 1-5, shouldn't be too hard?:"))
+                if guess > 5:
+                    print("Hmmm, could've sworn there was only five numbers on the keypad" + "\n")
+                    guesses +=1
+                elif guess < 1:
+                    print("Didn't know you could have negative numbers on a keypad" + "\n")
+                    guesses +=1
+                else:
 
-                #If/ Elif statements that check whether the user's guess is correct,
-                #then provides hints.
-                if guess == (number):
-                    print ("Ahaha! looks like we have got it right" + "\n")
-                    run += 1
-                    guesses = 5
-                    number = str(number)
-                    access_code = str(access_code) + number
-                    number = int(number)
-                    if run == 4:
-                        completed = True
-                elif guesses > 2:
-                    print("INTRUDER ALERT,INTRUDER ALERT, RESETTING CODE NOW..." + "\n")
-                    return access_code_minigame()
-                elif guess > number:
-                    print ("Something feels wrong about this guess. It must be lower" + "\n")
-                    guesses +=1
-                elif guess < number:
-                    print ("Hmmm, My spidy senses tells me this number is higher" + "\n")
-                    guesses +=1
+                    #If/ Elif statements that check whether the user's guess is correct,
+                    #then provides hints.
+                    if guess == (number):
+                        print ("Ahaha! looks like we have got it right" + "\n")
+                        run += 1
+                        guesses = 5
+                        number = str(number)
+                        access_code = str(access_code) + number
+                        number = int(number)
+                        if run == 4:
+                            completed = True
+                    elif guesses > 2:
+                        print("INTRUDER ALERT,INTRUDER ALERT, RESETTING CODE NOW..." + "\n")
+                        return access_code_minigame()
+                    elif guess > number:
+                        print ("Something feels wrong about this guess. It must be lower" + "\n")
+                        guesses +=1
+                    elif guess < number:
+                        print ("Hmmm, My spidy senses tells me this number is higher" + "\n")
+                        guesses +=1
+            except ValueError:
+                print("There are only numbers on the keypad")
 
     #Setting variable values and telling the user the access code.                
     print("ACCESS CODE: " + access_code)                
@@ -72,4 +75,4 @@ def access_code_minigame():
             code_rejected += 1
 
 
-access_code_minigame()               
+access_code_minigame()        
