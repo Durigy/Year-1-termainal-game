3fb4f7e5841c17250b5860032da4e8e9fad19556
Th3Cl0udsRoot <litoiuig@cardiff.ac.uk>
2020-10-26 05:19:49 +0000

    Items
    Weight Added

--- main_game/items.py
+++ main_game/items.py
@@ -4,8 +4,9 @@ item_id = {
     "name": "id card",
 
     "description":
-    """You new shiny student ID card. Expires 1 June 2017.
-You wonder why they have printed a suicide hotline number on it?..."""
+        """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?...""",
+    "mass": "25"  # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_syringes_bag = {
@@ -14,8 +15,9 @@ item_syringes_bag = {
     "name": "syringes bag",
 
     "description":
-    """You new shiny student ID card. Expires 1 June 2017.
-You wonder why they have printed a suicide hotline number on it?..."""
+        """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?...""",
+    "mass": "600"  # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_batteries = {
@@ -24,10 +26,10 @@ item_batteries = {
     "name": "batteries",
 
     "description":
-    """You new shiny student ID card. Expires 1 June 2017.
-You wonder why they have printed a suicide hotline number on it?..."""
+        """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?...""",
+    "mass": "250"  # we need to adjust the mass, currently it is either too low or too high.
 }
-
 
 item_laptop = {
     "id": "laptop",
@@ -35,7 +37,8 @@ item_laptop = {
     "name": "laptop",
 
     "description":
-    "It has seen better days. At least it has a WiFi card!"
+        "It has seen better days. At least it has a WiFi card!",
+    "mass": "2500"  # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_money = {
@@ -44,7 +47,8 @@ item_money = {
     "name": "money",
 
     "description":
-    "This wad of cash is barely enough to pay your tuition fees."
+        "This wad of cash is barely enough to pay your tuition fees.",
+    "mass": "9000"  # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_biscuits = {
@@ -52,23 +56,26 @@ item_biscuits = {
 
     "name": "a pack of biscuits",
 
-    "description": "A pack of biscuits."
+    "description": "A pack of biscuits.",
+    "mass": "30"  # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_pen = {
     "id": "pen",
-    
+
     "name": "a pen",
 
-    "description": "A basic ballpoint pen."
+    "description": "A basic ballpoint pen.",
+    "mass": "125"  # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_handbook = {
     "id": "handbook",
-    
+
     "name": "a student handbook",
 
-    "description": "This student handbook explains everything. Seriously."
+    "description": "This student handbook explains everything. Seriously.",
+    "mass": "1000"  # we need to adjust the mass, currently it is either too low or too high.
 }
 
 tools = {
