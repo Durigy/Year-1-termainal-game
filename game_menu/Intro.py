d42dde7252bdf0778ba8c7a6dccd54ff904e602c
Th3Cl0udsRoot <litoiuig@cardiff.ac.uk>
2020-10-27 17:00:38 +0000

    game menu

---
+++ game_menu/Intro.py
@@ -0,0 +1,21 @@
+from asciimatics.effects import Cycle, Stars
+from asciimatics.renderers import FigletText
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+def demo(screen):
+    effects = [
+        Cycle(
+            screen,
+            FigletText("Safe Heaven", font='big'),
+            int(screen.height / 2 - 8)),
+        Cycle(screen,
+              FigletText("Press 'q' to start game.", font='small'),
+              int(screen.height / 1 - 8)
+              ),
+        Stars(screen, 300)
+    ]
+    screen.play([Scene(effects, 5)])
+Screen.wrapper(demo)
+
+#import replaceme replace, replaceme with the .py file.
+#replaceme
