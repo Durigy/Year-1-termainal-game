55f5e50a12ae15fd3b7d042c1664beb3195e809f
Alexander Deverson <deversonaa@cardiff.ac.uk>
2020-10-29 12:48:24 +0000

    Items.py update

--- main_game/items.py
+++ main_game/items.py
@@ -7,8 +7,6 @@ item_mask = {
     "description":
     """A mask, used in a pandemic a long time
 ago.""",
-
-    "mass": "20"
 }
 
 item_id_card = {
@@ -19,8 +17,6 @@ item_id_card = {
     "description":
     """An ID Card that can be used to get
 into Laboratory 2.""",
-
-    "mass": "20"
 }
 
 item_torch = {
@@ -34,8 +30,6 @@ in handy in dark places. Perhaps it could even
 be used as a weapon? It must have been at one
 point - the end of it is covered in dried
 blood.""",
-
-    "mass" : "25"  # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_syringes_bag = {
@@ -47,8 +41,6 @@ item_syringes_bag = {
     """A bag of syringes - clearly not sterilised.
 You wonder if they were used for something other than
 medicinal practices back when things were normal.""",
-
-    "mass" : "40" # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_batteries = {
@@ -60,8 +52,6 @@ item_batteries = {
     """A pack of batteries that will be suitable
 for a torch. It's amazing they haven't been taken
 already.""",
-
-    "mass" : "130" # we need to adjust the mass, currently it is either too low or too high.
 }
 
 
@@ -75,8 +65,6 @@ item_laptop = {
 technology, once upon a time. Maybe when the world
 returns to normal, there will be such thing as
 'new' technology again.""",
-
-    "mass" : "2500" # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_substance1 = {
@@ -88,8 +76,6 @@ item_substance1 = {
     """This checical substance is something familiar to
 Dr. Gavigan, but is rather elusive to you. One fact
 remains; it's vital for the vaccine.""",
-
-    "mass" : "120" # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_paper_passcode = {
@@ -101,8 +87,6 @@ item_paper_passcode = {
     """A piece of old, yellow paper with a passcode
 messily strewn on it. Whoever wrote this certainly
 has horrible handwriting.""",
-
-    "mass" : "20" # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_hammer = {
@@ -114,8 +98,6 @@ item_hammer = {
     """The hammer has clearly been used in self
 defence, but has remained in good shape. It'll do
 the trick of it's original purpose.""",
-
-    "mass" : "500" # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_cleaning_product = {
@@ -127,8 +109,6 @@ item_cleaning_product = {
     """The disinfectant spray is definitely a go-to
 for making disinfecting items easy. That is what it
 does, after all.""",
-
-    "mass" : "350" # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_axe = {
@@ -140,8 +120,6 @@ item_axe = {
     """This axe would've been a very handy weapon for
 someone outside of the gates. Unfortunately for them,
 it's here.""",
-
-    "mass" : "2300" # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_notebook = {
@@ -153,8 +131,6 @@ item_notebook = {
     """A notebook by Dr. Gavigan, containing important
 notes for the vaccine! Despite, he's informed you not to
 read it yourself.""",
-
-    "mass" : "400" # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_beakers = { 
@@ -165,8 +141,6 @@ item_beakers = {
     "description":
     """Beakers that can be used in the making of
 chemicals and scientific experiments.""",
-
-    "mass" : "100" # we need to adjust the mass, currently it is either too low or too high.
 }
 
 
@@ -179,8 +153,6 @@ item_substance2 = {
     """This checical substance is something familiar to
 Dr. Gavigan, but is rather elusive to you. One fact
 remains; it's vital for the vaccine.""",
-
-    "mass" : "50" # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_substance3 = {
@@ -192,8 +164,6 @@ item_substance3 = {
     """This checical substance is something familiar to
 Dr. Gavigan, but is rather elusive to you. One fact
 remains; it's vital for the vaccine.""",
-
-    "mass" : "60" # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_pen = { # this is new added and needs to be sorted
@@ -203,8 +173,6 @@ item_pen = { # this is new added and needs to be sorted
 
     "description":
         """A basic ballpoint pen.""",
-
-    "mass": "125" # we need to adjust the mass, currently it is either too low or too high.
 }
 
 
@@ -220,22 +188,18 @@ item_salted_crisps = {
 crisps go out of date.""",
 
     "energy" : 1, # we need to adjust the energy, currently it is either too low or too high.
-
-    "mass" : 10 # we need to adjust the mass, currently it is either too low or too high.
 }
 
-item_mars_bar = {
-    "id": "mars bar",
+item_chocolate_bar = {
+    "id": "chocolate bar",
 
-    "name": "Mars Bar",
+    "name": "Chocolate Bar",
 
     "description":
-    """A mars bar - which looks very tempting right
-about now. Will it taste just like it used to?""",
+    """A chocolate bar - which looks very tempting
+right about now. Will it taste just like it used to?""",
 
     "energy" : 3, # we need to adjust the energy, currently it is either too low or too high.
-
-    "mass" : 10 # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_canned_beans = {
@@ -250,8 +214,6 @@ beans on toast as your go-to meal when you were a
 university student.""",
 
     "energy" : 1, # we need to adjust the energy, currently it is either too low or too high.
-
-    "mass" : 10 # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_canned_potatoes = {
@@ -265,8 +227,6 @@ to find out they're well within their expiry date. A
 filling so-called meal, nowadays.""",
 
     "energy" : 1, # we need to adjust the energy, currently it is either too low or too high.
-
-    "mass" : 10 # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_pickles = {
@@ -281,8 +241,6 @@ probably won't notice if one jar disappears. Afterall,
 how else did it get here?""",
 
     "energy" : 1, # we need to adjust the energy, currently it is either too low or too high.
-
-    "mass" : 10 # we need to adjust the mass, currently it is either too low or too high.
 }
 
 item_vodka = {
@@ -296,14 +254,12 @@ the outside, but unopened - which will be useful as
 a make-shift disinfectant.""",
 
     "energy" : 1, # we need to adjust the energy, currently it is either too low or too high.
-
-    "mass" : 10 # we need to adjust the mass, currently it is either too low or too high.
 }
 
 # --------------------------------------------------------
 
 tools = {
-    "id card": item_id_card,
+    "id card": item_id_card
     "torch": item_torch,
     "syringes bag": item_syringes_bag,
     "batteries": item_batteries,
@@ -316,13 +272,13 @@ tools = {
     "beakers": item_beakers,
     "substance2": item_substance2,
     "substance3": item_substance3,
-    "pen": item_pen,
+    "pen": item_pen
     "mask": item_mask
 }
 
 food = {
     "salted crisps": item_salted_crisps,
-    "mars bar": item_mars_bar,
+    "chocolate bar": item_chocolate_bar,
     "canned beans": item_canned_beans,
     "canned potatoes": item_canned_potatoes,
     "pickles": item_pickles,
