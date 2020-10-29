b21846eca12d5c536b17a7b7145e7c7ee269a556
Luc Woodman <woodmanl1@cardiff.ac.uk>
2020-10-29 18:06:51 +0000

    Update __init__.py

--- main_game/__init__.py
+++ main_game/__init__.py
@@ -4,3 +4,4 @@ from .player import current_room, inventory #, characters
 from .items import tools, food
 from .gameparser import print_specialised_words, normalise_input, old_time
 from .User_Interface import header, footer, info
+from .minigames import access_code_minigame, scrambled_word, morse_code_minigame
